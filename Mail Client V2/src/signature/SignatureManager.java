556cdf01ddaa9e8b5dab75b85b2a7ae52185f4af
aleksandra49 <anja.9811@gmail.com>
2020-09-17 19:41:29 +0200

    Slucajno zamenjen sm sa smobject, izmenjene funkcije u klasiSugnatureManager

--- Mail Client V2/src/signature/SignatureManager.java
+++ Mail Client V2/src/signature/SignatureManager.java
@@ -23,44 +23,57 @@ public class SignatureManager {
 	 * @param data - podaci koji treba digitalno da se potpisu
 	 * @param privateKey - privatni kljuc sa kojim se podaci digitalno potpisuju
 	 * 
-	 * @return Objekat koji sadrzi podatke koji se potpisuju, privatni kljuc kojim se potpisuje i digitalni potpis
+	 * @return Digitalni potpis
 	 */
-	public SignedObject sign(byte[] data, PrivateKey privateKey) {
+	public byte[] sign(byte[] data, PrivateKey privateKey) {
 		try {
 			// kreiramo objekat za potpisivanje
 			// parametar getInstance metode je algoritam koji se koristi za potpisivanje. Na primer. vrednost moze biti i "SHA1withDSA"
 			// voditi racuna da se koristi algoritam za potpisivanje i za proveru potpisa!
 			Signature signature = Signature.getInstance("SHA1withRSA");
 			
-			// kreira se objekat koji sadrzi podatke koji se potpisuju, privatni kljuc kojim se potpisuje i digitalni potpis
-			return new SignedObject(data, privateKey, signature);
-		} catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException | IOException e) {
+			// inicijalizacija privatnim kljucem - POTPISUJE SE PRIVATNIM KLJCEM!!!
+			signature.initSign(privateKey);
+			
+			// postavljamo podatke koje potpisujemo
+			signature.update(data);
+
+			// vrsimo potpisivanje
+			return signature.sign();
+		} catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
 			e.printStackTrace();
 		}
 		
 		return null;
 	}
-	
+
 	/**
 	 * Metoda za verifikaciju digitalnog potpisa.
 	 * 
-	 * @param signedObject - objekat koji sadrzi podatke koji se potpisuju, privatni kljuc kojim se potpisuje i digitalni potpis
+	 * @param data - originalni podaci
+	 * @param digitalSignature - digitalni potpis podataka
 	 * @param publicKey - javni kljuc sa kojim se vrsi provera digitalnog potpisa
 	 * 
 	 * @return Boolean vrednost koja oznacava da li je digitalni potpis validan, odnosno da podaci nisu u medjuvremenu menjani
 	 * 
 	 * NAPOMENA: metoda ce vratiti true samo ukoliko originalni podaci nisu menjanu i ukoliko se za verifikaciju koristi javni kljuc
-	 * koji je par sa privatnim kljucem sa kojim su podaci potpisani
+	 * koji je par sa privatnim kljucem sa kojim su podaci potpisani ž
 	 */
-	public boolean verify(SignedObject signedObject, PublicKey publicKey) {
+	public boolean verify(byte[] data, byte[] digitalSignature, PublicKey publicKey) {
 		try {
 			// kreiramo objekat za potpisivanje
 			// parametar getInstance metode je algoritam koji se koristi za potpisivanje. Na primer. vrednost moze biti i "SHA1withDSA"
 			// voditi racuna da se koristi algoritam za potpisivanje i za proveru potpisa!
 			Signature signature = Signature.getInstance("SHA1withRSA");
 						
+			// inicijalizacija privatnim kljucem - POTPIS SE VERIFIKUJE JAVNIM KLJUCEM!!!
+			signature.initVerify(publicKey);
+			
+			// postavljamo podatke koje potpisujemo
+			signature.update(data);
+
 			// vrsimo proveru potpisa
-			return signedObject.verify(publicKey, signature);
+			return signature.verify(digitalSignature);
 		} catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
 			e.printStackTrace();
 		}
